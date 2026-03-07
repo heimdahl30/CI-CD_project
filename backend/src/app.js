1b2d8287b9612080892b497ef6f57a78a57cef28
ABHINAV SINGH <abhinavsingh@ABHINAVs-MacBook-Air.local>
2026-03-07 21:13:22 +0530

    debugging done hopefully

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,12 +13,12 @@ const app = express()
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
-.then(result => {
-    logger.info("Connected to MongoDB")
-})
-.catch(error => {
-    logger.error("error connecting to MongoDB:", error.message)
-})
+    .then(result => {
+        logger.info("Connected to MongoDB")
+    })
+    .catch(error => {
+        logger.error("error connecting to MongoDB:", error.message)
+    })
 
 app.use(express.static('dist'))
 app.use(express.json())
@@ -28,6 +28,11 @@ app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
