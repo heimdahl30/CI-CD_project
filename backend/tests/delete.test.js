9ac89d2771786a66b88e0e5b9639b8ad663ea012
ABHINAV SINGH <abhinavsingh@ABHINAVs-MacBook-Air.local>
2026-03-09 11:19:39 +0530

    file sructure corrected

--- backend/tests/delete.test.js
+++ backend/tests/delete.test.js
@@ -2,23 +2,23 @@ const { test, after } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-const app = require('../app')
+const app = require('../src/app')
 const api = supertest(app)
 
 test('delete a blog', async () => {
-    
+
     const initialBlogs = await api.get('/api/blogs')
     const initialBlogsLength = initialBlogs.body.length
     const blogToDelete = initialBlogs.body[5]
 
     await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
 
     const finalBlogs = await api.get('/api/blogs')
     const finalBlogsLength = finalBlogs.body.length
 
-    assert.strictEqual(finalBlogsLength,initialBlogsLength - 1)
+    assert.strictEqual(finalBlogsLength, initialBlogsLength - 1)
 })
 
 after(async () => {
