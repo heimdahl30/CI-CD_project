edd45995358f71b1134659350a7cad0d2444c039
ABHINAV SINGH <abhinavsingh@ABHINAVs-MacBook-Air.local>
2026-03-10 21:19:02 +0530

    too tired

--- frontend/playwright_blogtesting/playwright.config.js
+++ frontend/playwright_blogtesting/playwright.config.js
@@ -78,7 +78,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:5173',
+    url: 'http://127.0.0.1:5173',
     cwd: '../',
     reuseExistingServer: !process.env.CI,
   },
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3003",
+        target: "http://127.0.0.1:3003",
         changeOrigin: true,
       },
     },
