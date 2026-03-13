1e20086161964f5d3c276a668f5a00991a21920b
ABHINAV SINGH <abhinavsingh@ABHINAVs-MacBook-Air.local>
2026-03-13 12:11:00 +0530

    added SECRET sauce

--- backend/src/utils/middleware.js
+++ backend/src/utils/middleware.js
@@ -43,6 +43,7 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
+
   logger.error(error.message)
 
   if (error.name === 'CastError') {
