23fdd774f8f5fd880b96c7e68fb8b41663b781a6
ABHINAV SINGH <abhinavsingh@ABHINAVs-MacBook-Air.local>
2026-03-14 21:22:11 +0530

    removed a certain middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,10 +35,6 @@ if (process.env.NODE_ENV === 'test') {
   app.use('/api/testing', testingRouter)
 }
 
-app.get('(.*)', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
-
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
