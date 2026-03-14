1b251b8a5d1618a6fd48550a45e53dd2d4dd5f50
ABHINAV SINGH <abhinavsingh@ABHINAVs-MacBook-Air.local>
2026-03-14 21:19:55 +0530

    changed * to (.*)

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'test') {
   app.use('/api/testing', testingRouter)
 }
 
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
