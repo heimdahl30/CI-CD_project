1b13bc5d6c7fbd4f1bff5a50dc7c6f6401fa02b7
ABHINAV SINGH <abhinavsingh@ABHINAVs-MacBook-Air.local>
2026-03-16 09:40:41 +0530

    added buil_step.sh and a new route in the backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,6 +35,10 @@ if (process.env.NODE_ENV === 'test') {
   app.use('/api/testing', testingRouter)
 }
 
+app.get('/*path', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
