d5175e5ad412b6135fdce757007ce8dba94e2442
ABHINAV SINGH <abhinavsingh@ABHINAVs-MacBook-Air.local>
2026-03-17 20:27:35 +0530

    deployment

--- frontend/playwright_blogtesting/playwright.config.js
+++ frontend/playwright_blogtesting/playwright.config.js
@@ -78,12 +78,13 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
+  /* webServer: {
     command: 'npm run dev -- --host 127.0.0.1',
     timeout: 120 * 1000,
     url: 'http://127.0.0.1:5173',
     cwd: '../',
     reuseExistingServer: !process.env.CI,
   },
+  */
 })
 
